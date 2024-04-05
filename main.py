1947e02b760359578e1be6a371a310d285a228bd
AlexD717 <157652245+AlexD717@users.noreply.github.com>
2024-04-05 22:15:04 +0000

    Combat Development Started

--- main.py
+++ main.py
@@ -3,33 +3,44 @@ import random
 import pickle
 import os.path
 
+
 class Enemy:
+
   def __init__(self, health, ac, damage, attackModifier):
     self.health = health
     self.damage = damage
     self.attackModifier = attackModifier
     self.ac = ac
 
+
 class Weapon:
+
   def __init__(self, name, damage, cost):
     self.name = name
     self.damage = damage
     self.cost = cost
 
+
 class Player:
+
   def __init__(self, name, attackModifier, weaponList, money):
     self.name = name
     self.attackModifier = attackModifier
     self.weaponList = weaponList
     self.money = money
+
   def __reduce__(self):
-    return (self.__class__, (self.name, self.attackModifier, self.weaponList, self.money))
+    return (self.__class__, (self.name, self.attackModifier, self.weaponList,
+                             self.money))
+
 
 aviableWeapons = [Weapon("Dagger", 1, 1), Weapon("Short Sword", 3, 5)]
+
 
 def SaveData(allPlayers, aviableWeapons):
   with open('playerData.pkl', 'wb') as file:
     pickle.dump([allPlayers, aviableWeapons], file)
+
 
 def LoadData():
   global aviableWeapons
@@ -45,7 +56,8 @@ def LoadData():
       print("The weapons " + player.name + " has are")
       for weapon in player.weaponList:
         print(weapon.name + " that does " + str(weapon.damage) + " damage")
-     
+
+
 def AskQuestion(questionText, validResults):
   userAnswer = input(questionText)
   if (userAnswer in validResults):
@@ -58,16 +70,20 @@ def AskQuestion(questionText, validResults):
     time.sleep(0.5)
     return AskQuestion(questionText, validResults)
 
+
 def StartArea():
   time.sleep(1)
   print("\nYou are at the front of the dungeon")
   time.sleep(0.5)
-  userResponse = AskQuestion("Whould you like to go visit the shop or enter the dungeon (shop/dungeon)? ", ["shop", "dungeon"])
+  userResponse = AskQuestion(
+      "Whould you like to go visit the shop or enter the dungeon (shop/dungeon)? ",
+      ["shop", "dungeon"])
   if (userResponse == "shop"):
     print("You are visiting the shop")
     Shop()
   else:
     Dungeon()
+
 
 def BuyItem(aviableItems, player):
   itemNames = [item.name for item in aviableItems]
@@ -76,20 +92,28 @@ def BuyItem(aviableItems, player):
   if (player.money >= itemBuying.cost):
     player.money -= itemBuying.cost
     player.weaponList.append(itemBuying)
-    print("You bought a " + itemBuying.name + " for " + str(itemBuying.cost) + " coins")
+    print("You bought a " + itemBuying.name + " for " + str(itemBuying.cost) +
+          " coins")
     print("You now have " + str(player.money) + " coins")
     print(len(player.weaponList))
   else:
     print("You don't have enough money to buy that")
-  userChoice = AskQuestion("Whould you like to buy anything else? ", ["yes", "no"])
+  userChoice = AskQuestion("Whould you like to buy anything else? ",
+                           ["yes", "no"])
   if (userChoice == "yes"):
-    BuyItem(aviableItems, player)    
+    BuyItem(aviableItems, player)
+
 
 shopWeapons = []
+
+
 def RandomiseShopItems():
   global shopWeapons
   shopWeapons = [random.choices(aviableWeapons, k=5)][0]
+
+
 RandomiseShopItems()
+
 
 def Shop():
   time.sleep(1)
@@ -97,20 +121,41 @@ def Shop():
   time.sleep(0.2)
   print("Here are your weapon choices")
   for weapon in shopWeapons:
-    print("This weapon is a " + weapon.name + " that does " + str(weapon.damage) + " damage and costs " + str(weapon.cost) + " coins.")
+    print("This weapon is a " + weapon.name + " that does " +
+          str(weapon.damage) + " damage and costs " + str(weapon.cost) +
+          " coins.")
   for player in allPlayers:
-    userChoice = AskQuestion("Whould " + player.name + " like to buy anything? ", ["yes", "no"])
+    userChoice = AskQuestion(
+        "Whould " + player.name + " like to buy anything? ", ["yes", "no"])
     if (userChoice == "yes"):
       BuyItem(shopWeapons, player)
   print("Come back later when the items will be refreshed")
   StartArea()
 
+
+'''
+roomEnemies = [[], []]
+highestRoomBeat = 0
 def Dungeon():
+  global highestRoomBeat
   time.sleep(1)
   print("\nYou have entered the dungeon")
+  room = 0
+  while (room < len(roomEnemies)):
+    TurnCombat(allPlayers, roomEnemies[room])
+    room += 1
+    if (room > highestRoomBeat):
+      highestRoomBeat
+
+def TurnCombat(allPlayers, roomEnemies):
+  print("The enmies in this room are " + " ,".joing(roomEnemies))
+  targetEnemy = AskQuestion("Which Enemy would you like to attack? ", )
+'''
+
 
 def AskToLoadData():
-  loadData = AskQuestion("Whould you like to load your previous data? ", ["yes", "no"])
+  loadData = AskQuestion("Whould you like to load your previous data? ",
+                         ["yes", "no"])
   if (loadData == "yes"):
     LoadData()
     StartArea()
@@ -121,15 +166,18 @@ def AskToLoadData():
     else:
       CreatePlayers()
 
+
 def CreatePlayers():
   global allPlayers
-  numPlayers = int(AskQuestion("How many people are playing? ", ["1", "2", "3", "4"]))
+  numPlayers = int(
+      AskQuestion("How many people are playing? ", ["1", "2", "3", "4"]))
   allPlayers = []
   for i in range(numPlayers):
     playerName = input("What is the name of player " + str(i + 1) + "? ")
     allPlayers.append(Player(playerName, 0, [Weapon("Dagger", 1, 1)], 0))
   SaveData(allPlayers, aviableWeapons)
   StartArea()
+
 
 if (os.path.isfile("./playerData.pkl")):
   AskToLoadData()
