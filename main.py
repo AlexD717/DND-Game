56c031425a36508455ae77b7552e7d959e4cd600
AlexD717 <157652245+AlexD717@users.noreply.github.com>
2024-04-12 16:59:08 +0000

    Bug Fixes

--- main.py
+++ main.py
@@ -192,6 +192,7 @@ def LoadData():
     for player in allPlayers:
       player.playerClass.setPlayerLevel()
       print(f"{player.name} who is a {player.playerClass.name} on level {player.playerClass.level} with {player.coins} coins")
+      print(f"They have {player.health} health and {player.ac} ac")
       print(f"The weapons {player.name} has are")
       for weapon in player.weaponList:
         if (weapon.number > 0):
@@ -280,17 +281,11 @@ def Shop():
   print("Here are your item choices")
   for item in shopItems:
     if item.type == "Weapon":
-      print(
-          f"This weapon is a {item.name} that does {item.damage} damage on the frontline, {item.rdamage} on the backline, and costs {item.cost} coins."
-      )
+      print(f"This weapon is a {item.name} that does {item.damage} damage on the frontline, {item.retreatDamage} on the backline, and costs {item.cost} coins.")
     elif item.type == "Potion":
-      print(
-          f"This {item.name} is a potion that restores {item.healthMod} health, increases your ac by {item.acMod}, increases your attack by {item.attackMod}, increases your shield by {item.shield} and costs {item.cost}"
-      )
+      print(f"This {item.name} is a potion that restores {item.healthMod} health, increases your ac by {item.acMod}, increases your attack by {item.attackMod}, increases your shield by {item.shieldMod} and costs {item.cost}")
     elif item.type == "Armor":
-      print(
-          f"This {item.name} is a piece of armor that increases your ac by {item.acMod}, and increases your shield by {item.shieldMod}, costing {item.cost} coins"
-      )
+      print(f"This {item.name} is a piece of armor that increases your ac by {item.acMod}, and increases your shield by {item.shieldMod}, costing {item.cost} coins")
   for player in allPlayers:
     #remove this
     player.coins = 10000
@@ -309,7 +304,7 @@ def SellItems(player: Player):
     print("These are your items")
     userChoices = []
     for weapon in player.weaponList:
-      print(f"{weapon.name} is a weapon that does {weapon.damage} damage and can be sold for {math.floor(weapon.cost2)}")
+      print(f"{weapon.name} is a weapon that does {weapon.damage} damage and can be sold for {math.floor(weapon.cost)}")
       userChoices.append(weapon.name)
     userChoices.append("cancel")
     userChoice = AskQuestion(
@@ -320,11 +315,8 @@ def SellItems(player: Player):
       coinsGotBack = math.floor(weaponSelling.cost / 2)
       player.coins += coinsGotBack
       player.weaponList.remove(weaponSelling)
-      print(
-          f"{player.name} sold their {weaponSelling.name} for {coinsGotBack} coins."
-      )
-      userChoice = AskQuestion("Whould you like to sell another weapon? ",
-                               ["yes", "no"])
+      print(f"{player.name} sold their {weaponSelling.name} for {coinsGotBack} coins.")
+      userChoice = AskQuestion("Whould you like to sell another weapon? ", ["yes", "no"])
       if (userChoice == "yes"):
         SellItems(player)
       else:
@@ -433,9 +425,7 @@ def enemyAttack(enemy, playersInCombat):
       if len(allPlayers) == 0:
         return allPlayers
     else:
-      print(
-          f"{playerAttacking.name} now has {playerAttacking.healthTemp} health"
-      )
+      print(f"{playerAttacking.name} now has {playerAttacking.healthTemp} health")
       time.sleep(0.5)
   else:
     print(f"The {enemy.name}'s attack missed due to a skill issue")
@@ -447,8 +437,7 @@ def usePotion(player):
   potionNames = [potion.name for potion in player.potionList]
   for potion in potionNames:
     print(potion)
-  userChoice = AskQuestion("Which potions would you like to use? ",
-                           potionNames)
+  userChoice = AskQuestion("Which potions would you like to use? ", potionNames)
   potionUsing = player.potionList[potionNames.index(userChoice)]
   player.healthTemp += potionUsing.healthMod
   player.acTemp += potionUsing.acMod
@@ -501,17 +490,13 @@ def Combat(allPlayers, roomEnemies):
       else:
         raise Exception("Invalid Player status")
       if len(acceptableWeaponList) != 0:
-        userChoice = AskQuestion(
-            f"Which enemy would {player.name} like to attack? You are currently {player.status}. Alternatively, choose to disengage/re-engage. ",
-            enemyNames)
+        userChoice = AskQuestion(f"Which enemy would {player.name} like to attack? You are currently {player.status}. Alternatively, choose to disengage/re-engage. ", enemyNames)
         if userChoice == "disengage":
           print(f"{player.name} has decided to try and run away.")
           player.status = "disengaging"
           continue
         if userChoice == "re-engage":
-          print(
-              f"{player.name} has regained some courage and has decided to re-engage"
-          )
+          print(f"{player.name} has regained some courage and has decided to re-engage")
           player.status = "engaged"
           continue
         enemyAttacking = currentRoomEnemies[enemyNames.index(userChoice)]
@@ -525,9 +510,7 @@ def Combat(allPlayers, roomEnemies):
             print(f"{weapon.name} that does {weapon.retreatDamage} damage")
         else:
           raise Exception("Invalid Player Status")
-        userChoice = AskQuestion(
-            "Which weapon would you like to use? ",
-            [weapon.name for weapon in acceptableWeaponList])
+        userChoice = AskQuestion("Which weapon would you like to use? ", [weapon.name for weapon in acceptableWeaponList])
         weaponUsing = player.weaponList[weaponNames.index(userChoice)]
         time.sleep(1)
         currentRoomEnemies, player.coins = playerAttack(
@@ -536,23 +519,17 @@ def Combat(allPlayers, roomEnemies):
           return True
       else:
         userChoice = AskQuestion(
-            "You have no weapons that can be used currently. Would you like to disengage/re-engage? ",
-            ["disengage", "re-engage", "no"])
+            "You have no weapons that can be used currently. Would you like to disengage/re-engage? ", ["disengage", "re-engage", "no"])
         if userChoice == "disengage":
           print(f"{player.name} has decided to try and run away")
           player.status = "disengaging"
         elif userChoice == "re-engage":
-          print(
-              f"{player.name} has regained some courage and has decided to re-engage"
-          )
+          print(f"{player.name} has regained some courage and has decided to re-engage")
           player.status = "engaged"
       if player.status == "disengaged" and len(player.potionList) > 0:
-        userChoice = AskQuestion("Would you like to use a potion? ",
-                                 ["yes", "no"])
+        userChoice = AskQuestion("Would you like to use a potion? ", ["yes", "no"])
         if userChoice == "yes":
-          player.healthTemp, player.acTemp, player.attackModifierTemp, player.shieldTemp, player.potionList = usePotion(
-              player)
-
+          player.healthTemp, player.acTemp, player.attackModifierTemp, player.shieldTemp, player.potionList = usePotion(player)
   playersInCombat = [
       player for player in allPlayers if player.status != "disengaged"
   ]
@@ -577,13 +554,10 @@ def AttackHit(attackMod, ac):
   elif (roll == 20) or roll + attackMod >= ac:
     return True
   else:
-    raise Exception(
-        "Something went wrong calculating if the attack hit or not")
-
+    raise Exception("Something went wrong calculating if the attack hit or not")
 
 def AskToLoadData():
-  loadData = AskQuestion("Whould you like to load your previous data? ",
-                         ["yes", "no"])
+  loadData = AskQuestion("Whould you like to load your previous data? ", ["yes", "no"])
   if (loadData == "yes"):
     LoadData()
     StartArea()
@@ -596,7 +570,6 @@ def AskToLoadData():
 
 
 aviableClassNames = ["Fighter", "Ranger", "Paladin"]
-
 
 def CreatePlayers():
   global allPlayers
